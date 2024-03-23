6ded968404f3f943604279152954ac8569616fdc
tuanliaaaa <nhattuan44t@gmail.com>
2024-03-23 13:02:38 +0700

    activity hotelDetail

---
+++ app/src/main/java/com/example/hotel_booking/DetailHotelActivity.java
@@ -0,0 +1,14 @@
+package com.example.hotel_booking;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class DetailHotelActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailhotel);
+    }
+}
